8a65f37140a1d7c620f630d6457ea8f1cda69802
Naima Hassan <naima.hassan434@gmail.com>
2022-07-01 17:21:21 +0100

    React Router - Setup

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
@@ -19,7 +20,9 @@ const Landing = () => {
             aliquam, dolores itaque tempore in doloremque quaerat incidunt minus
             velit maxime!
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
