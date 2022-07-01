068618bb217e5ce96067b84f2b676df8bcf3e980
Naima Hassan <naima.hassan434@gmail.com>
2022-07-01 12:56:10 +0100

    Landing Page - Structure

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,7 +3,28 @@ import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            aliquam, dolores itaque tempore in doloremque quaerat incidunt minus
+            velit maxime!
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
