d118a0f936cffffca054c9bfd544d17244043dd9
Naima Hassan <naima.hassan434@gmail.com>
2022-07-24 11:12:45 +0100

    Navbar Structure

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -4,7 +4,7 @@ import Wrapper from "../assets/wrappers/SmallSidebar";
 const SmallSidebar = () => {
   return (
     <Wrapper>
-      <div>SmallSidebar</div>;
+      <div>SmallSidebar</div>
     </Wrapper>
   );
 };
