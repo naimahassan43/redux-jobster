fa66b914010c6eac91b169d6f7fa0f4567cb4d54
Naima Hassan <naima.hassan434@gmail.com>
2022-07-28 13:16:26 +0100

    BigSidebar

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Wrapper from "../assets/wrappers/BigSidebar";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const BigSidebar = () => {
-  return <Wrapper>BigSidebar</Wrapper>;
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen
+            ? "sidebar-container "
+            : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default BigSidebar;
