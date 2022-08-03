1712b52772fd8c5d22b2c04fbea955d537b0772f
Naima Hassan <naima.hassan434@gmail.com>
2022-08-03 17:57:43 +0100

    Container Setup

--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Wrapper from "../assets/wrappers/JobsContainer";
+import Job from "./Job";
+import Loading from "./Loading";
 
 const JobsContainer = () => {
-  return <div>JobsContainer</div>;
+  const { jobs, isLoading } = useSelector((store) => store.allJobs);
+  const dispatch = useDispatch();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <h5>jobs info</h5>
+      <div className="jobs">
+        {jobs.map((job) => {
+          console.log(job);
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default JobsContainer;
